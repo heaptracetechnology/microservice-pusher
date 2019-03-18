28483029e9b8b08dbee769f6b8c143bfa8fe6705
ht68rohit <rohits@heaptrace.com>
2019-03-18 13:08:44 +0530

    minor changes

--- result/result.go
+++ result/result.go
@@ -9,7 +9,6 @@ import (
 func WriteErrorResponse(responseWriter http.ResponseWriter, err error) {
 	msgbytes, _ := json.Marshal(err)
 	WriteJsonResponse(responseWriter, msgbytes, http.StatusBadRequest)
-	return
 }
 
 func WriteJsonResponse(responseWriter http.ResponseWriter, bytes []byte, code int) {
