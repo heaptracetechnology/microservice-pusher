74c2855a9df4ba12995521e1a60ca3bfc4e8af7a
ht68rohit <rohits@heaptrace.com>
2019-03-18 13:05:16 +0530

    Fixed issues from GolangCI

--- messaging/message_test.go
+++ messaging/message_test.go
@@ -5,39 +5,44 @@ import (
 	"encoding/json"
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
+	"log"
 	"net/http"
 	"net/http/httptest"
 	"os"
 )
 
 type TestArgsData struct {
-	AppId           string      `json:"appid"`
-	Cluster         string      `json:"cluster"`
-	Channel			string		`json:"channel"`
-	Event			string		`json:"event"`
-	Title			string		`json:"title"`
-	Message			string		`json:"message"`
+	AppId   string `json:"appid"`
+	Cluster string `json:"cluster"`
+	Channel string `json:"channel"`
+	Event   string `json:"event"`
+	Title   string `json:"title"`
+	Message string `json:"message"`
 }
 
 var _ = Describe("Pusher messaging", func() {
 
 	testmessage := TestArgsData{
-		AppId: "728602",
+		AppId:   "728602",
 		Cluster: "ap2",
-		Channel :"my-channel1",
-		Event:"my-event",
-		Title :"Sports",
-		Message:"Cricket",
+		Channel: "my-channel1",
+		Event:   "my-event",
+		Title:   "Sports",
+		Message: "Cricket",
 	}
 
-	reqbody := new(bytes.Buffer)
-	json.NewEncoder(reqbody).Encode(testmessage)
+	requestBody := new(bytes.Buffer)
+	errr := json.NewEncoder(requestBody).Encode(testmessage)
+	if errr != nil {
+		log.Fatal(errr)
+	}
 
 	os.Setenv("SECRET", "14b80ade884bc6e20261")
 	os.Setenv("KEY", "53961c904521c1807997")
 
-	req, err := http.NewRequest("POST", "/send", reqbody)
+	req, err := http.NewRequest("POST", "/send", requestBody)
 	if err != nil {
+		log.Fatal(err)
 	}
 	recorder := httptest.NewRecorder()
 	handler := http.HandlerFunc(SendMessage)
@@ -52,26 +57,59 @@ var _ = Describe("Pusher messaging", func() {
 	})
 })
 
-
 var _ = Describe("Pusher messaging negative", func() {
 
 	testmessage := TestArgsData{
-		AppId: "728602",
+		AppId:   "728602",
 		Cluster: "ap2",
-		Channel :"my-channel1",
-		Event:"my-event",
-		Title :"Sports",
-		Message:"Cricket",
+		Channel: "my-channel1",
+		Event:   "my-event",
+		Title:   "Sports",
+		Message: "Cricket",
 	}
 
-	reqbody := new(bytes.Buffer)
-	json.NewEncoder(reqbody).Encode(testmessage)
+	requestBody := new(bytes.Buffer)
+	errr := json.NewEncoder(requestBody).Encode(testmessage)
+	if errr != nil {
+		log.Fatal(errr)
+	}
 
 	os.Setenv("SECRET", "14b80ade884bc6e20261")
 	os.Setenv("KEY", "53961c904521c180799709800")
 
-	req, err := http.NewRequest("POST", "/send", reqbody)
+	req, err := http.NewRequest("POST", "/send", requestBody)
 	if err != nil {
+		log.Fatal(err)
+	}
+	recorder := httptest.NewRecorder()
+	handler := http.HandlerFunc(SendMessage)
+	handler.ServeHTTP(recorder, req)
+
+	Describe("Send message", func() {
+		Context("SendMessage", func() {
+			It("Should result http.StatusOK", func() {
+				Expect(recorder.Code).To(Equal(http.StatusBadRequest))
+			})
+		})
+	})
+})
+
+var _ = Describe("Pusher messaging negative unmarshal ", func() {
+
+	testmessage := []byte(`{"status":false}`)
+
+	requestBody := new(bytes.Buffer)
+	errr := json.NewEncoder(requestBody).Encode(testmessage)
+	if errr != nil {
+		log.Fatal(errr)
+	}
+
+	os.Setenv("SECRET", "14b80ade884bc6e20261")
+	os.Setenv("KEY", "53961c904521c180799709800")
+
+	req, err := http.NewRequest("POST", "/send", requestBody)
+	if err != nil {
+		log.Fatal(err)
 	}
 	recorder := httptest.NewRecorder()
 	handler := http.HandlerFunc(SendMessage)
--- messaging/messaging.go
+++ messaging/messaging.go
@@ -1,67 +1,73 @@
 package messaging
 
-import ("github.com/pusher/pusher-http-go"
-"github.com/heaptracetechnology/microservice-pusher/result"
-"net/http"
-"os"
-"io/ioutil"
-"encoding/json"
+import (
+	"encoding/json"
+	"github.com/heaptracetechnology/microservice-pusher/result"
+	"github.com/pusher/pusher-http-go"
+	"io/ioutil"
+	"net/http"
+	"os"
 )
 
 type ArgsData struct {
-	AppId           string      `json:"appid"`
-	Cluster         string      `json:"cluster"`
-	Channel			string		`json:"channel"`
-	Event			string		`json:"event"`
-	Title			string		`json:"title"`
-	Message			string		`json:"message"`
+	AppId   string `json:"appid"`
+	Cluster string `json:"cluster"`
+	Channel string `json:"channel"`
+	Event   string `json:"event"`
+	Title   string `json:"title"`
+	Message string `json:"message"`
 }
 
 type Message struct {
-    Success string `json:"success"`
-    Message string `json:"message"`
-	StatusCode int `json:"statuscode"`
+	Success    string `json:"success"`
+	Message    string `json:"message"`
+	StatusCode int    `json:"statuscode"`
 }
 
 type Data struct {
-    Title 	string `json:"title"`
-    Message string `json:"message"`
+	Title   string `json:"title"`
+	Message string `json:"message"`
 }
 
-func SendMessage(w http.ResponseWriter, r *http.Request){
+func SendMessage(w http.ResponseWriter, r *http.Request) {
 
 	var SECRET = os.Getenv("SECRET")
 	var KEY = os.Getenv("KEY")
 
 	body, err := ioutil.ReadAll(r.Body)
-	defer r.Body.Close()
-
-	var argsdata ArgsData
-	err = json.Unmarshal(body, &argsdata)
-
-	
-  	client := pusher.Client{
-		AppId: argsdata.AppId,
-		Key: KEY,
-		Secret: SECRET,
-		Cluster: argsdata.Cluster,
-		Secure: true,
-	  }	
-	  
-	data  := Data{
-		Title	: argsdata.Title,
-		Message : argsdata.Message,
-	}
-	_, err = client.Trigger(argsdata.Channel, argsdata.Event, data)
-
 	if err != nil {
 		result.WriteErrorResponse(w, err)
 		return
 	}
 
-	message := Message{"true","notification sent",http.StatusOK}
+	defer r.Body.Close()
+
+	var argsdata ArgsData
+	er := json.Unmarshal(body, &argsdata)
+	if er != nil {
+		result.WriteErrorResponse(w, er)
+		return
+	}
+
+	client := pusher.Client{
+		AppId:   argsdata.AppId,
+		Key:     KEY,
+		Secret:  SECRET,
+		Cluster: argsdata.Cluster,
+		Secure:  true,
+	}
+
+	data := Data{
+		Title:   argsdata.Title,
+		Message: argsdata.Message,
+	}
+	_, triggerErr := client.Trigger(argsdata.Channel, argsdata.Event, data)
+	if triggerErr != nil {
+		result.WriteErrorResponse(w, triggerErr)
+		return
+	}
+
+	message := Message{"true", "notification sent", http.StatusOK}
 	bytes, _ := json.Marshal(message)
 	result.WriteJsonResponse(w, bytes, http.StatusOK)
-	return
-
 }
